1996e858d00d9f7eced05d856f4872281f41c553
Antony Gilo <51474865+gilosupreme@users.noreply.github.com>
2025-04-15 10:44:28 +0300

    First commi - customized popups on map click

---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+if ('geolocation' in navigator) {
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const map = L.map('map').setView([latitude, longitude], 16);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (e) {
+        const { lat, lng } = e.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('You worked out here')
+          .openPopup();
+      });
+    },
+    () => console.log('Location Not available')
+  ),
+    {
+      maximumAge: 0,
+      timeout: 3000,
+      enableHighAccuracy: true,
+    };
+} else {
+  console.log('Geolocation is not supported by your browser.');
+}
