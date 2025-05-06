9e6bada1c9998ac5916c727d08288646b8d85b1f
Antony Gilo <51474865+gilosupreme@users.noreply.github.com>
2025-05-06 16:53:00 +0300

    Refactoring code into a class and making methods private

--- script.js
+++ script.js
@@ -10,66 +10,84 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-let map, mapEvent;
 
-if ('geolocation' in navigator) {
-  navigator.geolocation.getCurrentPosition(
-    position => {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+class App {
+  #map;
+  #mapEvent;
 
-      map = L.map('map').setView([latitude, longitude], 16);
+  constructor() {
+    this._getPosition();
 
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+    form.addEventListener('submit', this._newWorkOut.bind(this));
 
-      map.on('click', function (e) {
-        mapEvent = e; //reassigning the global event object to match the one passed in this callback
-        form.classList.remove('hidden');
-        inputDistance.focus();
-      });
-    },
-    () => console.log('Location Not available')
-  ),
-    {
-      maximumAge: 0,
-      timeout: 3000,
-      enableHighAccuracy: true,
-    };
-} else {
-  console.log('Geolocation is not supported by your browser.');
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+
+  _getPosition() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), () =>
+        console.log('Location Not available')
+      ),
+        {
+          maximumAge: 0,
+          timeout: 3000,
+          enableHighAccuracy: true,
+        };
+    } else {
+      console.log('Geolocation is not supported by your browser.');
+    }
+  }
+
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    this.#map = L.map('map').setView([latitude, longitude], 16);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(e) {
+    this.#mapEvent = e; //reassigning the global event object to match the one passed in this callback
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkOut(fe) {
+    fe.preventDefault();
+
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        ''; //clearing the field values after submit
+
+    const { lat, lng } = this.#mapEvent.latlng;
+
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('You worked out here')
+      .openPopup();
+  }
 }
 
-form.addEventListener('submit', fe => {
-  fe.preventDefault();
-
-  inputDistance.value =
-    inputDuration.value =
-    inputCadence.value =
-    inputElevation.value =
-      ''; //clearing the field values after submit
-
-  const { lat, lng } = mapEvent.latlng;
-
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-      })
-    )
-    .setPopupContent('You worked out here')
-    .openPopup();
-});
-
-inputType.addEventListener('change', () => {
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-});
+const app = new App();
